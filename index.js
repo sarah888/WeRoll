6704f9b2435d45ecf190fd42b064570f1fff9026
sarah888 <shuan392@uwo.ca>
2023-01-14 23:04:31 -0500

    working to standards, fixed textboxes!

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ const updateLocalTrips = (localTripData) => {
     localTripCard.className = "column"
 
     localTripCard.innerHTML = (
-        `<div class="column">
+        `<div class="card">
         <h3>${"Trip to " + localTripData.destination}</h3>
-        <p>${"From: " + localTripData.start + "\nSeats Remaining: " + (6 - (localTripData.passengers)) + (
+        <p>${"From: " + localTripData.start + "\n@"+ localTripData.hour+":"+localTripData.minute 
+        +"\nSeats Remaining: " + (6 - (localTripData.passengers)) + (
             localTripData.wheelchair == "on" ? "\nWheelchair ramp included" : ""
         ) + (
             localTripData.caregiver == "on" ? "\nCaregiver included" : ""
-        )}</p>
+        )}</p><br/><br/>
         </div>`
     );
 
@@ -52,31 +53,3 @@ const rideFormHandler = (event) => {
 }
 
 addEventListener('submit', rideFormHandler);
-
-
-
-// // Get the modal
-// var modal = document.getElementById("myModal");
-
-// // Get the button that opens the modal
-// var btn = document.getElementById("myBtn");
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// // When the user clicks the button, open the modal 
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// }
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
